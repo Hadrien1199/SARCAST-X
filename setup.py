cc2cff881407824e977b5e5cf8134c34ca7864ae
Hadrien Bage <hadrienbage@gmail.com>
2024-03-19 18:04:34 +0100

    Update: .gitignore, requirements.txt, setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='solAIre',
+      version="0.0.0",
+      description="Solar Flare Prediction (from SDO Benchmark)",
+    #   install_requires=requirements,
+      packages=find_packages(),
+      test_suite="tests",
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      zip_safe=False)
